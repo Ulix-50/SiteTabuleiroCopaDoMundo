b238d5abc53dcdcbf818ed11f967d3a48d4d0735
Ulix-50 <118901475+Ulix-50@users.noreply.github.com>
2022-11-24 00:37:16 -0300

    VersaoDeTestePosAula
    Versão de Teste realizada em casa após a aula (24/11/22)

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,50 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 300,
+    height: 300,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 }
+        }
+    },
+    scene: {
+        init: Init,
+        preload: Preload,
+        create: Create,
+        update: Update
+    }
+};
 
-var texto;
-var valor= 1;
+const game = new Phaser.Game(config);
+
+var valor = 1;
+var ImagemDado;
+
+function Init() {
+}
 
 function Preload() {
-    this.load.image('Dado_Lado1', 'ASSETS/DadoLado1.jpg');
-    this.load.image('Dado_Lado2', 'ASSETS/DadoLado2.jpg');
-    this.load.image('Dado_Lado3', 'ASSETS/DadoLado3.jpg');
-    this.load.image('Dado_Lado4', 'ASSETS/DadoLado4.jpg');
-    this.load.image('Dado_Lado5', 'ASSETS/DadoLado5.jpg');
-    this.load.image('Dado_Lado6', 'ASSETS/DadoLado6.jpg');
+    this.load.image('Dado_Lado1', 'ASSETS/DadoLado1.png');
+    this.load.image('Dado_Lado2', 'ASSETS/DadoLado2.png');
+    this.load.image('Dado_Lado3', 'ASSETS/DadoLado3.png');
+    this.load.image('Dado_Lado4', 'ASSETS/DadoLado4.png');
+    this.load.image('Dado_Lado5', 'ASSETS/DadoLado5.png');
+    this.load.image('Dado_Lado6', 'ASSETS/DadoLado6.png');
 }
 
 function Create() {
-    this.add.image(150,150,'Dado_Lado1');
-    
+
+    DadoLado1 = this.add.image(150, 150,'Dado_Lado1').setInteractive();
+    DadoLado2 = this.add.image(150, 150,'Dado_Lado2').setInteractive();
+    DadoLado3 = this.add.image(150, 150,'Dado_Lado3').setInteractive();
+    DadoLado4 = this.add.image(150, 150,'Dado_Lado4').setInteractive();
+    DadoLado5 = this.add.image(150, 150,'Dado_Lado5').setInteractive();
+    DadoLado6 = this.add.image(150, 150,'Dado_Lado6').setInteractive();
+
     this.input.on('pointerdown', AdicionarValor);
-    this.input.on('pointerup', DefinirValor);
+    
+    
 }
 
 function Update() {
@@ -23,46 +52,81 @@ function Update() {
 }
 
 function AdicionarValor() {
-    valor = Math.floor(Math.random() * 7);    
+    valor = Math.floor(Math.random() * 7);
     if (valor == 0) {
         valor = 1;
     }
     DefinirLadoDoDado(valor)
-    texto.setText(valor);
-}
-
-function DefinirValor() {
-    valor = valor;
 }
 
 function DefinirLadoDoDado(Valor) {
     switch (Valor) {
         case 1:
-            remove('Dado_Lado1');
+            CarregarImagem_Dado(DadoLado1);
+            ApagarImagem_Dado(DadoLado2);
+            ApagarImagem_Dado(DadoLado3);
+            ApagarImagem_Dado(DadoLado4);
+            ApagarImagem_Dado(DadoLado5);
+            ApagarImagem_Dado(DadoLado6);
             break;
-    
+        case 2:
+            ApagarImagem_Dado(DadoLado1);
+            CarregarImagem_Dado(DadoLado2);
+            ApagarImagem_Dado(DadoLado3);
+            ApagarImagem_Dado(DadoLado4);
+            ApagarImagem_Dado(DadoLado5);
+            ApagarImagem_Dado(DadoLado6);
+            break;
+        case 3:
+            ApagarImagem_Dado(DadoLado1);
+            ApagarImagem_Dado(DadoLado2);
+            CarregarImagem_Dado(DadoLado3);
+            ApagarImagem_Dado(DadoLado4);
+            ApagarImagem_Dado(DadoLado5);
+            ApagarImagem_Dado(DadoLado6);
+            break;
+        case 4:
+            ApagarImagem_Dado(DadoLado1);
+            ApagarImagem_Dado(DadoLado2);
+            ApagarImagem_Dado(DadoLado3);
+            CarregarImagem_Dado(DadoLado4);
+            ApagarImagem_Dado(DadoLado5);
+            ApagarImagem_Dado(DadoLado6);
+            break;
+        case 5:
+            ApagarImagem_Dado(DadoLado1);
+            ApagarImagem_Dado(DadoLado2);
+            ApagarImagem_Dado(DadoLado3);
+            ApagarImagem_Dado(DadoLado4);
+            CarregarImagem_Dado(DadoLado5);
+            ApagarImagem_Dado(DadoLado6);
+            break;
+        case 6:
+            ApagarImagem_Dado(DadoLado1);
+            ApagarImagem_Dado(DadoLado2);
+            ApagarImagem_Dado(DadoLado3);
+            ApagarImagem_Dado(DadoLado4);
+            ApagarImagem_Dado(DadoLado5);
+            CarregarImagem_Dado(DadoLado6);
+            break;
         default:
+            CarregarImagem_Dado(DadoLado1);
+            ApagarImagem_Dado(DadoLado2);
+            ApagarImagem_Dado(DadoLado3);
+            ApagarImagem_Dado(DadoLado4);
+            ApagarImagem_Dado(DadoLado5);
+            ApagarImagem_Dado(DadoLado6);
             break;
     }
 }
 
-const config = {
+function CarregarImagem_Dado(LadoDado) {
+    LadoDado.visible = true;
+    console.log(valor + " foi selecionado");
+    
+}
 
-    type: Phaser.AUTO,
-
-    width: 300,
-    height: 300,
-    physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: { y: 300 }
-      }
-    },
-    scene: {
-        preload: Preload,
-        create: Create,
-        update: Update,
-    }
-};
-
-const game = new Phaser.Game(config);
+function ApagarImagem_Dado(LadoDado) {
+    LadoDado.visible = false;
+    
+}
